aad3697b54cbe9540fcc2f477e8742a01fedf348
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-09-12 16:29:44 +0900

    feat: try to draw mandelbrot but not working due to unreachable error in wasm

---
+++ src/drawer.ts
@@ -0,0 +1,12 @@
+export const X_MIN = -1.5
+export const X_MAX = 0.5
+export const Y_MIN = -1.0
+export const Y_MAX = 1.0
+export const MAX_ITER = 64
+
+const draw = (context: CanvasRenderingContext2D, canvasWidth: number, canvasHeight: number, data: Uint8Array): void => {
+  const img = new ImageData(new Uint8ClampedArray(data), canvasWidth, canvasHeight)
+  context.putImageData(img, 0, 0)
+}
+
+export default draw
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,52 @@
+import draw, { MAX_ITER, X_MAX, X_MIN, Y_MAX, Y_MIN } from './drawer'
+
 import('../wasm/mandelbrot/pkg/mandelbrot')
   .catch((err) => {
     console.error(err)
   })
   .then((modules) => {
     if (!modules) return
-    const { greet } = modules
-    greet()
+    const { generate_mandelbrot_set, draw_mandelbrot_set } = modules
+    const generateMandelbrotSetWasm = (
+      canvasWidth: number,
+      canvasHeight: number,
+      xMin: number,
+      xMax: number,
+      yMin: number,
+      yMax: number,
+      maxIter: number
+    ) => generate_mandelbrot_set(canvasWidth, canvasHeight, xMin, xMax, yMin, yMax, maxIter)
+
+    const drawMandelbrotSetWasm = () => draw_mandelbrot_set()
+
+    return { generateMandelbrotSetWasm, drawMandelbrotSetWasm }
+  })
+  .then(({ generateMandelbrotSetWasm, drawMandelbrotSetWasm }) => {
+    console.log('finished loading wasm')
+    const renderButton = document.getElementById('render')
+    renderButton.addEventListener('click', () => {
+      // draw with wasm
+      {
+        console.log('start generating drawing with wasm')
+        drawMandelbrotSetWasm()
+      }
+      // draw with hybrid
+      {
+        console.log('start generating with wasm and drawing with js')
+        const CANVAS_ID = 'canvas_hybrid'
+        const canvas = document.getElementById(CANVAS_ID) as HTMLCanvasElement
+        const context = canvas.getContext('2d')
+        const canvasWidth = canvas.width
+        const canvasHeight = canvas.height
+
+        const generateStartTime = Date.now()
+        const wasmResult = generateMandelbrotSetWasm(canvasWidth, canvasHeight, X_MIN, X_MAX, Y_MIN, Y_MAX, MAX_ITER)
+        const generateElapsedTime = Date.now() - generateStartTime
+        const drawStartTime = Date.now()
+        draw(context, canvasWidth, canvasHeight, wasmResult)
+        const drawElapsedTime = Date.now() - drawStartTime
+        console.log(`\tgenerated:wasm\tgenerate_elapsed:${generateElapsedTime}ms`)
+        console.log(`\tdraw:  js\tdraw_elapsed:${drawElapsedTime}ms`)
+      }
+    })
   })
